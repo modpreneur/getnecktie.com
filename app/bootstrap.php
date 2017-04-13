3ff656d867cad9caac0b9bf48f7f617f2286a2fd
MartinDrozdek <martin.drozdek@gmail.com>
2017-04-13 08:08:17 +0200

    debug on local ip

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+$configurator->setDebugMode('128.0.178.14'); // enable for your remote IP
 $configurator->setDebugMode(false);
 $configurator->enableTracy(__DIR__ . '/../log');
 
